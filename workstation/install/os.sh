9de05dc48dbafb9b91ad7feca697c37bb5c86e82
Alex Peats-Bond <alexpb.tgm@gmail.com>
2020-09-09 23:01:50 -0400

    os: Add vscode, iterm2 to brew install

--- workstation/install/os.sh
+++ workstation/install/os.sh
@@ -6,5 +6,4 @@ xcode-select --install
 # brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 brew upgrade
-brew tap homebrew/cask-cask
-brew cask install spectacle
+brew cask install iterm2 spectacle visual-studio-code
