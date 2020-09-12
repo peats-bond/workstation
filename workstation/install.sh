ad0f75fc50113875bf37dd0957954e0455ade5af
Alex Peats-Bond <alexpb.tgm@gmail.com>
2020-09-12 13:40:32 -0400

    install: consolidate install scripts

--- workstation/install.sh
+++ workstation/install.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
-source install/os.sh
-source install/git.sh
-source install/cmd.sh
+shell="emacs tig"
+navigation="rg fasd tree direnv"
+analysis="mitmproxy htop cloc colordiff"
+dev="markdown shellcheck"
+
+# xcode
+xcode-select --install
+
+# git
+git config --global core.excludesfile ~/.gitignore_global
+
+# zprezto
+git submodule update --init --recursive 
+
+# brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+brew upgrade
+
+brew cask install iterm2 spectacle visual-studio-code slack chrome
+echo "$shell $navigation $analysis $dev"  | xargs brew install
+
+# language
 source install/golang.sh
+source install/python.sh
--- workstation/install/os.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# xcode command line tools
-xcode-select --install
-
-# brew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-brew upgrade
-brew cask install iterm2 spectacle visual-studio-code slack
