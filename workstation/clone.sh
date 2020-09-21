652891eb056dbd63f965fd2212b25fddb1e705fb
Alex Peats-Bond <alexpb.tgm@gmail.com>
2020-09-20 23:41:54 -0400

    clone: update github username to "peats-bond"

--- workstation/clone.sh
+++ workstation/clone.sh
@@ -2,6 +2,6 @@
 
 # cloning workstation into home directory
 git init .
-git remote add origin git@github.com:AlexAPB/workstation.git
+git remote add origin git@github.com:peats-bond/workstation.git
 git pull origin master
 git branch --set-upstream-to=origin/master master
